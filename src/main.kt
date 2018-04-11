cfdea90d4e77edfacca5c368222ff18f022eea6a
munstein <eduardo.munstein@live.com>
2018-04-10 22:23:32 -0300

    Implementing main.kt

--- src/main.kt
+++ src/main.kt
@@ -1,1 +1,10 @@
-fun main(args: Array<String>) {}
+fun main(args: Array<String>) {
+    var person = Person("Stringson", 20, 'x')
+
+    with(person) {
+        println("Name: " + name)
+        println("Age: " + age)
+        println("Gender: " + gender)
+    }
+
+}
